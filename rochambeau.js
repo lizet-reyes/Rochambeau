5002407952a142f93afb3321ab915078e0149f2d
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-06-01 12:14:31 -0700

    almost done

--- rochambeau.js
+++ rochambeau.js
@@ -3,6 +3,13 @@
 var playerChoice;
 var computerChoice;
 
+// Stores the lables for the choices
+var choices = ["Rock", "Paper", "Scissors"];
+
+// Variable to store the score
+// score[0] = wins, score[1] = ties, score[2] = losses
+var score = [0,0,0];
+
 // Stores the player's choice, then call's the function for storing the computer's choice
 function storePlayerChoice(choice) {
     playerChoice = choice;
@@ -21,20 +28,61 @@ function playGame(){
     // Here is the game ruleset algorithm
     if (playerChoice == computerChoice) {
         // We have a tie!
-        console.log("tie");
+        updateScore(1);
+        displayGameResult("tie")
     } else if (playerChoice == 0 && computerChoice == 2) {
         // Rock beats scissors - a win!
-        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
     } else if (playerChoice == 1 && computerChoice == 0) {
         // Paper beats scissors - a win!
-        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
     } else if (playerChoice == 2 && computerChoice == 1) {
         // Scissors beats paper - a win!
-        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
     } else {
         // All other combinations are losses
-        console.log("lose");
+        updateScore(2);
+        displayGameResult("lose")
     }
+}
+
+//Displays the result of the game
+function displayGameResult(result){
+    // Define an array of text labels for the choices 0, 1, 2;
+    // Create a message for the player
+    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
+    // Add to the base message if it was a win, loss, or tie
+    if (result === "win") {
+        // Display that it was a win
+        document.getElementById("result").textContent = message + " YOU WIN!";
+        document.getElementById("result").className = "alert alert-success";
+    } else if (result === "lose") {
+        // Display that it was a loss
+        document.getElementById("result").textContent = message + " YOU LOSE!";
+        document.getElementById("result").className = "alert alert-danger";
+    } else {
+        // Display that it was a tie
+        document.getElementById("result").textContent = message + " A tie.";
+        document.getElementById("result").className = "alert alert-info";
+    }
+
+    updateScoreBoard();
+}
+
+// Updates the score
+function updateScore(val){
+    ++score[val];
+    console.log("The score is now " + score);
+}
+
+// Function for displaying the score
+function updateScoreBoard(){
+    document.getElementById("wins").textContent = score[0];
+    document.getElementById("losses").textContent = score[2];
+    document.getElementById("ties").textContent = score[1];
 }
 
 // The button elements
@@ -48,4 +96,3 @@ rockButton.addEventListener('click', () => {storePlayerChoice(0)});
 paperButton.addEventListener('click', () => {storePlayerChoice(1)});
 scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
 playButton.addEventListener('click', () => {playGame()});
-
